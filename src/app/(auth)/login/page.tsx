02d648ec94eadcf70ba653b74de348d863dd525b
ahmedyasser365 <ahmedyasser573he7@gmail.com>
2025-06-27 21:07:54 +0300

    f12

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,11 @@
- // src/app/(auth)/login/page.tsx
 
 import LoginForm from "@/app/_components/LoginForm";
-
- 
-
 export const metadata = {
   title: "Login Page - social app",
  icons: {
-    icon: "/favicon.png", // or /favicon.png, /my-icon.svg, etc.
-    
+    icon: "/favicon.png",
   },
-  description:'the msd you abbo ou jhhn my website social',
 };
-
 export default function LoginPage() {
   return <LoginForm />;
 }
