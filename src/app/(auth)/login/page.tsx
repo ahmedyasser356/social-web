6dc99e628c2ee8775d535073425e9b6d5f521e5d
ahmedyasser365 <ahmedyasser573he7@gmail.com>
2025-06-28 00:31:58 +0300

    fg12

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,14 @@
 
 import LoginForm from "@/app/_components/LoginForm";
+ 
 export const metadata = {
   title: "Login Page - social app",
  
 };
 export default function LoginPage() {
-  return <LoginForm />;
+  return (<>
+  
+  <LoginForm />
+   
+  </>);
 }
--- src/app/_components/Providers.tsx
+++ src/app/_components/Providers.tsx
@@ -7,13 +7,18 @@ import { Provider } from "react-redux";
 import { store } from "@/libs/store";
 import { Toaster } from "react-hot-toast";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import LoadingScreen from "./LoadingScreen";
+import Cookies from 'js-cookie'
+import { usePathname } from "next/navigation";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  let path = usePathname()
   return (
     <Provider store={store}>
       <AppRouterCacheProvider>
         <ThemeProvider theme={theme}>
           <Toaster />
+          {Cookies.get('token') ||path == '/login' || path=='/register' ?'':<LoadingScreen></LoadingScreen>}
           {children}
         </ThemeProvider>
       </AppRouterCacheProvider>
